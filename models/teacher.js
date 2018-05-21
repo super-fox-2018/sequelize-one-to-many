4629f188127a4bd646c49dcaac8f9058b71cecc9
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-22 06:46:51 +0700

    update sequelize one to many

---
+++ models/teacher.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Teacher = sequelize.define('Teacher', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    subject: DataTypes.STRING
+  }, {});
+  Teacher.associate = function(models) {
+    // associations can be defined here
+  };
+  return Teacher;
+};
